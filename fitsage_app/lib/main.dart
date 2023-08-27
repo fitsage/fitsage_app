6e9b2970ed784262a3467b9fadfc7b72b479d063
fitsage <fitsageteam@gmail.com>
2023-08-27 20:21:49 +0530

    signup page update

--- fitsage_app/lib/main.dart
+++ fitsage_app/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:fitsage_app/views/Signup.dart';
+import 'package:fitsage_app/views/SignIn.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const SignUpScreen());
+  runApp(const SignInScreen());
 }
